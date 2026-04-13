5e8bfc5eb661878e5bdfe8b68b83fe2ed6d063e8
Pablo Gallegos <tuttopro27@gmail.com>
2026-04-13 16:52:26 -0400

    Initial commit

---
+++ src/test/java/com/ats/postulant/AtsPostulantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.postulant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsPostulantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
